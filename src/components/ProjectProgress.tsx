e5f8ace4f88b0a715275bc8e5f20f61dd4da9bc1
SayanDeveloper <samantasayan23@gmail.com>
2023-04-06 18:03:06 +0530

    🔥 cleanup

--- src/components/ProjectProgress.tsx
+++ src/components/ProjectProgress.tsx
@@ -3,37 +3,6 @@ import { BsThreeDotsVertical } from 'react-icons/bs'
 
 const ProjectProgress = () => {
 
-  useEffect(() => {
-    const circularProgress = document.querySelectorAll<HTMLElement>(".circular-progress");
-
-    Array.from(circularProgress).forEach((progressBar) => {
-      
-      const progressValue = progressBar.querySelector(".percentage") as HTMLElement;
-      const innerCircle = progressBar.querySelector(".inner-circle") as HTMLElement;
-      let startValue = 0,
-        endValue = Number(progressBar.getAttribute("data-percentage")),
-        speed = 50,
-        progressColor = progressBar.getAttribute("data-progress-color");
-
-      if (!progressValue || !innerCircle) return
-      const progress = setInterval(() => {
-        startValue++;
-        progressValue.textContent = `${startValue}%`;
-        progressValue.style.color = `${progressColor}`;
-
-        innerCircle.style.backgroundColor = `${progressBar.getAttribute(
-          "data-inner-circle-color"
-        )}`;
-        progressBar.style.background = `conic-gradient(${progressColor} ${
-          startValue * 3.6
-        }deg,${progressBar.getAttribute("data-bg-color")} 0deg)`;
-        if (startValue === endValue) {
-          clearInterval(progress);
-        }
-      }, speed);
-    });
-  }, [])
-
   return (
     <div className='w-[50%] bg-[#2c2c2c] rounded-[28px] p-[20px] pt-[28px] h-[258px]'>
       <div className='flex justify-between items-center'>
